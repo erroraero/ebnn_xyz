7e8ce4a66216ddb50222b096f535f150b3625382
aparnah <avrxtdev@gmail.com>
2026-04-01 23:07:12 +0530

    Delete DISABLE_RLS_FOR_DEV.sql

--- DISABLE_RLS_FOR_DEV.sql
+++
@@ -1,7 +0,0 @@
--- TEMPORARY: Disable RLS for development to allow updates without a real session
-ALTER TABLE profiles DISABLE ROW LEVEL SECURITY;
-ALTER TABLE links DISABLE ROW LEVEL SECURITY;
-
--- Allow everything on the storage (be CAREFUL, only for local dev)
--- Run this if you have problems with uploads:
--- create policy "Dev Allow All" on storage.objects for all using (true) with check (true);
